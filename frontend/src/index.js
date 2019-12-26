707627a29a64ef5a641ddd9592a2c4411cf42413
Kopylov <vladkopylov3@gmail.com>
2019-12-26 12:20:20 +0200

    configure styled-components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<div>Start</div>, document.getElementById('app'));
+ReactDOM.render(<div>Start</div>, document.getElementById('app'))
 
 if (module.hot) {
-    module.hot.accept()  
+    module.hot.accept()
 
     module.hot.addStatusHandler(status => {
         if (status === 'prepare') console.clear()
